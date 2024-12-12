8e3496b3eef35705b181d042ff2b6a52d43fb1d5
Ulrich Eck <ulrich.eck@tum.de>
2024-12-12 17:28:16 +0100

    updated to conan 2 and cpython from conan-center

--- conanfile.py
+++ conanfile.py
@@ -1,31 +1,86 @@
 import os
 
-from conans import ConanFile, tools
+from conan import ConanFile
+from conan.tools.files import copy, get, chdir
+from conan.tools.layout import basic_layout
+from conan.tools.env import Environment
+
+required_conan_version = ">=1.52.0"
 
 
 class CythonConan(ConanFile):
+    python_requires = "camp_common/0.5@camposs/stable"
+    python_requires_extend = "camp_common.CampPythonBase"
+
     name = "cython"
-    version = tools.get_env("GIT_TAG", "0.29.16")
+    version = "3.0.11-1"
     license = "Apache"
     description = "Cython language for python3"
+
     settings = "os", "compiler", "build_type", "arch"
 
-    def source(self):
-        tools.get("https://github.com/cython/cython/archive/{0}.tar.gz".format(self.version))
+
+    options = { 
+        "python": ["ANY"],
+        "python_version": [None, "3.12", ],
+        "with_system_python": [True, False],
+    }
+
+    default_options = {
+        "python": "python3",
+        "python_version": "3.12",
+        "with_system_python": False,
+    }
+
+    @property
+    def pyver(self):
+        pyver = self.options.python_version
+        if self.options.with_system_python:
+            pyver = ".".join(self._python_version.split(".")[1:2])
+        return pyver
+
+    @property
+    def python_lib_path(self):
+        return os.path.join(self.package_folder, "lib", f"python{self.pyver}", "site-packages")
+    
+    @property
+    def active_python_exec(self):
+        if not self.options.with_system_python:
+            cpython = self.dependencies["cpython"]
+            return os.path.join(cpython.package_folder, "bin", "python")
+        return self._python_exec
 
     def build_requirements(self):
-        self.build_requires("generators/1.0.0@camposs/stable")
+        if not self.options.with_system_python:
+            self.requires("cpython/[~{}]".format(self.options.python_version))
+            self.build_requires("python-pip/24.3.1@camposs/stable")
+            self.build_requires("python-setuptools/75.6.0@camposs/stable")
 
-    def requirements(self):
-        self.requires("python/[>=3.8.2]@camposs/stable")
-        self.requires("python-setuptools/[>=41.2.0]@camposs/stable")
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
+    def generate(self):
+        env1 = Environment()
+        env1.define("PYTHONPATH", self.python_lib_path)
+        envvars = env1.vars(self)
+        envvars.save_script("py_env_file")
+
+    def package_id(self):
+        self.info.clear()
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def build(self):
-        py_path = os.path.join(self.package_folder, "lib", "python3.8", "site-packages")
-        env = {"PYTHONPATH": os.environ["PYTHONPATH"] + os.pathsep + py_path}
-        os.makedirs(py_path)
-        with tools.chdir("%s-%s" % (self.name, self.version)), tools.environment_append(env):
-            self.run('python3 setup.py install --optimize=1 --prefix= --root="%s"' % self.package_folder)
+        if not os.path.isdir(self.python_lib_path):
+            os.makedirs(self.python_lib_path)
+        with chdir(self, self.source_folder):
+            self.run('{0} -m pip install --prefix= --root="{1}" .'.format(self.active_python_exec, self.package_folder))
+
+    def package(self):
+        pass
 
     def package_info(self):
-        self.env_info.PYTHONPATH.append(os.path.join(self.package_folder, "lib", "python3.8", "site-packages"))
+        self.runenv_info.append_path("PYTHONPATH", os.path.join(self.package_folder, "lib", f"python{self.pyver}", "site-packages"))
+        self.buildenv_info.append_path("PYTHONPATH", os.path.join(self.package_folder, "lib", f"python{self.pyver}", "site-packages"))
+
